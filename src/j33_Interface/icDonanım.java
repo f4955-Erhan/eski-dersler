2192e45a610100cd76b60fa94d6822cfcfc7b55b
f4955-Erhan <erhanyldzpls@gmail.com>
2022-11-18 15:23:25 +0300

    erhan main

---
+++ src/j33_Interface/icDonanım.java
@@ -0,0 +1,13 @@
+package j33_Interface;
+
+public interface icDonanım {
+
+    abstract void koltuk();// abs. meth-> public abstract
+
+    public void klima();// abs. meth
+
+    static final String MUSIC = "arizona kertenkele style :)underground";
+    //String  kumas ;//final variable initial edilmeli CTE
+    String KUMAS = "kadife";// yazmasakta hem final hem de static veriable
+    String RENK = "ahsap";
+}
